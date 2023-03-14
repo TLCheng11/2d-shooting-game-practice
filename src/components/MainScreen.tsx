21a878d527ba6a6bbc3e212e96251d95660df64b
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2023-03-14 19:08:30 -0400

    MainScreen canvas, setup ref and get context

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,9 +1,22 @@
+import { useLayoutEffect, useRef, useState } from "react";
 import css from "../styles/MainScreen.module.css";
 
 function MainScreen() {
+  const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>();
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useLayoutEffect(() => {
+    if (canvasRef.current) {
+      const c = canvasRef.current.getContext("2d");
+      setCtx(c);
+      canvasRef.current.width = 500;
+      canvasRef.current.height = 500;
+    }
+  }, []);
+
   return (
-    <div id={css.canvas1}>
-      <canvas id="canvas1"></canvas>
+    <div>
+      <canvas id={css.canvas1} ref={canvasRef}></canvas>
     </div>
   );
 }

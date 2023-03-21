39531d7956ae0691f95e70f410b808e4d5978d9b
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2023-03-21 19:47:02 -0400

    stop animation loop when gameover

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -30,16 +30,18 @@ function MainScreen() {
 
   /** an endless loop to refresh the canvas */
   function animate(timeStamp: number): void {
-    // use deltaTime to record time between each frame
-    const deltaTime = timeStamp - currentTime.current;
-    currentTime.current = timeStamp;
+    if (!game?.isGameOver) {
+      // use deltaTime to record time between each frame
+      const deltaTime = timeStamp - currentTime.current;
+      currentTime.current = timeStamp;
 
-    game?.update(deltaTime);
-    if (canvasRef.current && ctx) {
-      // clear canvas before each loop
-      ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-      game?.draw(ctx);
-      requestAnimationFrame(animate);
+      game?.update(deltaTime);
+      if (canvasRef.current && ctx) {
+        // clear canvas before each loop
+        ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+        game?.draw(ctx);
+        requestAnimationFrame(animate);
+      }
     }
   }
 

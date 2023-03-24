79ae5f4d3b3cf7d7c4674ebdc014bb9663f0c546
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2023-03-24 12:32:00 -0400

    add css module for GameBackground

--- src/components/GameBackground.tsx
+++ src/components/GameBackground.tsx
@@ -1,10 +1,12 @@
+import css from "../styles/GameBackground.module.css";
+
 function GameBackground() {
   return (
     <div>
-      <img id="layer1" src="./assets/background/layer1.png" alt="layer1" />
-      <img id="layer2" src="./assets/background/layer2.png" alt="layer2" />
-      <img id="layer3" src="./assets/background/layer3.png" alt="layer3" />
-      <img id="layer4" src="./assets/background/layer4.png" alt="layer4" />
+      <img id={css.layer1} src="./assets/background/layer1.png" alt="layer1" />
+      <img id={css.layer2} src="./assets/background/layer2.png" alt="layer2" />
+      <img id={css.layer3} src="./assets/background/layer3.png" alt="layer3" />
+      <img id={css.layer4} src="./assets/background/layer4.png" alt="layer4" />
     </div>
   );
 }
